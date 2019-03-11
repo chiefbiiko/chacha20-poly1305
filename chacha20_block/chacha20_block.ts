1b5120c9a31b3254aba3aa869c3d9756294fb75b
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-03-11 10:44:53 +0100

    chacha20_block initial state caching

--- chacha20_block/chacha20_block.ts
+++ chacha20_block/chacha20_block.ts
@@ -7,6 +7,8 @@ import {
   fourLittleEndianBytesToNumber
 } from "./../util/util.ts";
 
+const KEY_STATE: Map<Uint8Array, Uint32Array> = new Map();
+
 export function chaCha20Block(
   key: Uint8Array,
   nonce: Uint8Array,
@@ -25,24 +27,30 @@ export function chaCha20Block(
   if (out.length !== 64) {
     throw new TypeError("out must have 64 bytes");
   }
-  const state = new Uint32Array(16);
-  state[0] = 0x61707865;
-  state[1] = 0x3320646e;
-  state[2] = 0x79622d32;
-  state[3] = 0x6b206574;
-  state[4] = fourLittleEndianBytesToNumber(key, 0);
-  state[5] = fourLittleEndianBytesToNumber(key, 4);
-  state[6] = fourLittleEndianBytesToNumber(key, 8);
-  state[7] = fourLittleEndianBytesToNumber(key, 12);
-  state[8] = fourLittleEndianBytesToNumber(key, 16);
-  state[9] = fourLittleEndianBytesToNumber(key, 20);
-  state[10] = fourLittleEndianBytesToNumber(key, 24);
-  state[11] = fourLittleEndianBytesToNumber(key, 28);
-  state[12] = counter & 0xffffffff;
-  state[13] = fourLittleEndianBytesToNumber(nonce, 0);
-  state[14] = fourLittleEndianBytesToNumber(nonce, 4);
-  state[15] = fourLittleEndianBytesToNumber(nonce, 8);
-  const initialState: Uint32Array = Uint32Array.from(state);
+  let initialState: Uint32Array;
+  if (KEY_STATE.has(key)) {
+    initialState = KEY_STATE.get(key);
+  } else {
+    initialState = new Uint32Array(16);
+    initialState[0] = 0x61707865;
+    initialState[1] = 0x3320646e;
+    initialState[2] = 0x79622d32;
+    initialState[3] = 0x6b206574;
+    initialState[4] = fourLittleEndianBytesToNumber(key, 0);
+    initialState[5] = fourLittleEndianBytesToNumber(key, 4);
+    initialState[6] = fourLittleEndianBytesToNumber(key, 8);
+    initialState[7] = fourLittleEndianBytesToNumber(key, 12);
+    initialState[8] = fourLittleEndianBytesToNumber(key, 16);
+    initialState[9] = fourLittleEndianBytesToNumber(key, 20);
+    initialState[10] = fourLittleEndianBytesToNumber(key, 24);
+    initialState[11] = fourLittleEndianBytesToNumber(key, 28);
+    initialState[12] = counter & 0xffffffff;
+    initialState[13] = fourLittleEndianBytesToNumber(nonce, 0);
+    initialState[14] = fourLittleEndianBytesToNumber(nonce, 4);
+    initialState[15] = fourLittleEndianBytesToNumber(nonce, 8);
+    KEY_STATE.set(key, initialState);
+  }
+  const state: Uint32Array = Uint32Array.from(initialState);
   let i: number;
   for (i = 0; i < 10; ++i) {
     chaCha20QuarterRound(state, 0, 4, 8, 12);
--- chacha20_block/chacha20_block_test.ts
+++ chacha20_block/chacha20_block_test.ts
@@ -4,28 +4,31 @@ import { hex2bin } from "./../util/util.ts";
 
 const { readFileSync } = Deno;
 
-export function loadTestVector(): {
+interface TestVector {
   key: Uint8Array;
   nonce: Uint8Array;
   counter: number;
   expected: Uint8Array;
-} {
-  const testVector = JSON.parse(
-    new TextDecoder().decode(readFileSync("./chacha20_block_test_vector.json"))
+}
+
+export function loadTestVectors(): TestVector[] {
+  const testVectors = JSON.parse(
+    new TextDecoder().decode(readFileSync("./chacha20_block_test_vectors.json"))
   );
-  return {
+  return testVectors.map((testVector: { [key: string]: any }): TestVector => ({
     key: hex2bin(testVector.key),
     nonce: hex2bin(testVector.nonce),
     counter: testVector.counter,
     expected: hex2bin(testVector.expected)
-  };
+  }));
 }
 
 test(function chaCha20Blocking(): void {
-  const { key, nonce, counter, expected } = loadTestVector();
   const actual: Uint8Array = new Uint8Array(64);
-  chaCha20Block(key, nonce, counter, actual);
-  assert.equal(actual, expected);
+  for (const { key, nonce, counter, expected } of loadTestVectors()) {
+    chaCha20Block(key, nonce, counter, actual);
+    assert.equal(actual, expected);
+  }
 });
 
 runIfMain(import.meta);
