930caa2a06343683145b1f084498b3c20936775b
nschwarz <noah_anabiik.schwarz@daimler.com>
2019-03-14 17:44:47 +0100

    adjust quarter round test vectors to allow multiple

--- chacha20_quarter_round/chacha20_quarter_round_test.ts
+++ chacha20_quarter_round/chacha20_quarter_round_test.ts
@@ -3,32 +3,35 @@ import { chaCha20QuarterRound } from "./chacha20_quarter_round.ts";
 
 const { readFileSync } = Deno;
 
-function loadTestVector(): {
+interface TestVector {
   initialState: Uint32Array;
   quarterRoundParameters: number[];
   expectedState: Uint32Array;
-} {
-  const testVector = JSON.parse(
+}
+
+function loadTestVectors(): TestVector[] {
+  const testVectors = JSON.parse(
     new TextDecoder().decode(
-      readFileSync("./chacha20_quarter_round_test_vector.json")
+      readFileSync("./chacha20_quarter_round_test_vectors.json")
     )
   );
-  return {
+  return testVectors.map((testVector: { [key: string]: any }): TestVector => ({
     initialState: Uint32Array.from(testVector.initialState),
     quarterRoundParameters: testVector.quarterRoundParameters,
     expectedState: Uint32Array.from(testVector.expectedState)
-  };
+  }));
 }
 
 test(function chaCha20QuarterRounding(): void {
-  const {
-    initialState,
-    quarterRoundParameters: [a, b, c, d],
-    expectedState
-  } = loadTestVector();
-  const state = Uint32Array.from(initialState);
-  chaCha20QuarterRound(state, a, b, c, d);
-  assert.equal(state, expectedState);
+  for (const {
+      initialState,
+      quarterRoundParameters: [a, b, c, d],
+      expectedState
+    } of loadTestVectors()) {
+    const state = Uint32Array.from(initialState);
+    chaCha20QuarterRound(state, a, b, c, d);
+    assert.equal(state, expectedState);
+  }
 });
 
 runIfMain(import.meta);
