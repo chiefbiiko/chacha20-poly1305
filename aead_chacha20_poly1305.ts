0d78e43e252c20a15fd7a733d0b24f5605a90ac9
nschwarz <noah_anabiik.schwarz@daimler.com>
2019-03-14 19:02:08 +0100

    aead_chacha20_poly1305 test vector updates

--- aead_chacha20_poly1305.ts
+++ aead_chacha20_poly1305.ts
@@ -4,40 +4,31 @@ import { zeroPad16 } from "./zero_pad16/zero_pad16.ts";
 import { poly1305 } from "./poly1305/poly1305.ts";
 import { numberToLittleEndianBytes, inspect } from "./util/util.ts";
 
-/**
-chacha20_aead_encrypt(aad, key, iv, constant, plaintext):
-    nonce = constant | iv
-    otk = poly1305_key_gen(key, nonce)
-    ciphertext = chacha20_encrypt(key, 1, nonce, plaintext)
-    mac_data = aad | pad16(aad)
-    mac_data |= ciphertext | pad16(ciphertext)
-    mac_data |= num_to_8_le_bytes(aad.length)
-    mac_data |= num_to_8_le_bytes(ciphertext.length)
-    tag = poly1305_mac(mac_data, otk)
-    return (ciphertext, tag)
-*/
+export const KEY_BYTES: number = 32;
+export const NONCE_BYTES: number = 12;
+export const PLAINTEXT_BYTES_MAX: bigint = 274877906880n;
+export const CIPHERTEXT_BYTES_MAX: bigint = 274877906896n;
+export const AAD_BYTES_MAX: bigint = 18446744073709551615n;
 
 export function aeadChaCha20Poly1305Seal(
   key: Uint8Array,
-  iv: Uint8Array,
-  constant: Uint8Array,
+  nonce: Uint8Array,
   plaintext: Uint8Array,
   aad: Uint8Array
 ): { ciphertext: Uint8Array; tag: Uint8Array } {
-  if (key.length !== 32) {
-    throw new TypeError("key must have 32 bytes");
+  if (key.length !== KEY_BYTES) {
+    throw new TypeError(`key must have ${KEY_BYTES} bytes`);
   }
-  if (iv.length !== 8) {
-    throw new TypeError("iv must have 8 bytes");
+  if (nonce.length !== NONCE_BYTES) {
+    throw new TypeError(`nonce must have ${NONCE_BYTES} bytes`);
   }
-  if (constant.length !== 4) {
-    throw new TypeError("constant must have 4 bytes");
+  if (plaintext.length > PLAINTEXT_BYTES_MAX) {
+    throw new TypeError(`plaintext must not have more than ${PLAINTEXT_BYTES_MAX} bytes`);
   }
-  const nonce: Uint8Array = new Uint8Array(12);
-  nonce.set(constant, 0);
-  nonce.set(iv, 4);
+  if (aad.length > AAD_BYTES_MAX) {
+    throw new TypeError(`aad must not have more than ${AAD_BYTES_MAX} bytes`);
+  }
   const otk: Uint8Array = poly1305KeyGen(key, nonce);
-inspect(otk, "otk")
   const ciphertext: Uint8Array = chaCha20Cipher(key, nonce, 1, plaintext);
   const paddedCiphertext: Uint8Array = zeroPad16(ciphertext);
   const paddedAad: Uint8Array = zeroPad16(aad);
@@ -58,8 +49,15 @@ inspect(otk, "otk")
     8,
     paddedAad.length + paddedCiphertext.length + 8
   );
-inspect(pac, "pac")
   const tag: Uint8Array = poly1305(otk, pac);
-inspect(tag, "tag")
   return { ciphertext, tag };
 }
+
+// export function aeadChaCha20Poly1305Open(
+// key: Uint8Array,
+// nonce: Uint8Array,
+// plaintext: Uint8Array,
+// aad: Uint8Array
+// ): Uint8Array {
+//
+// }
--- aead_chacha20_poly1305_test.ts
+++ aead_chacha20_poly1305_test.ts
@@ -4,47 +4,59 @@ import { hex2bin } from "./util/util.ts";
 
 const { readFileSync } = Deno;
 
-function loadTestVector(): {
+interface TestVector {
   key: Uint8Array;
-  iv: Uint8Array;
-  constant: Uint8Array;
+  nonce: Uint8Array;
   plaintext: Uint8Array;
   aad: Uint8Array;
   ciphertext: Uint8Array;
   tag: Uint8Array;
-} {
-  const testVector = JSON.parse(
+}
+
+function loadTestVectors(): { seal: TestVector[], open: TestVector[] } {
+  const testVectors = JSON.parse(
     new TextDecoder().decode(
-      readFileSync("./aead_chacha20_poly1305_test_vector.json")
+      readFileSync("./aead_chacha20_poly1305_test_vectors.json")
     )
   );
   return {
-    key: hex2bin(testVector.key),
-    iv: hex2bin(testVector.iv),
-    constant: hex2bin(testVector.constant),
-    plaintext: hex2bin(testVector.plaintext),
-    aad: hex2bin(testVector.aad),
-    ciphertext: hex2bin(testVector.ciphertext),
-    tag: hex2bin(testVector.tag)
+    seal: testVectors.seal.map((testVector: { [key: string]: string }): TestVector => ({
+        key: hex2bin(testVector.key),
+        nonce: hex2bin(testVector.nonce),
+        plaintext: hex2bin(testVector.plaintext),
+        aad: hex2bin(testVector.aad),
+        ciphertext: hex2bin(testVector.ciphertext),
+        tag: hex2bin(testVector.tag)
+      })),
+    open: testVectors.open.map((testVector: { [key: string]: string }): TestVector => ({
+        key: hex2bin(testVector.key),
+        nonce: hex2bin(testVector.nonce),
+        plaintext: hex2bin(testVector.plaintext),
+        aad: hex2bin(testVector.aad),
+        ciphertext: hex2bin(testVector.ciphertext),
+        tag: hex2bin(testVector.tag)
+      }))
   };
 }
 
+const testVectors: { seal: TestVector[], open: TestVector[] } = loadTestVectors();
+
 test(function aeadChaCha20Poly1305Sealing(): void {
-  const {
-    key,
-    iv,
-    constant,
-    plaintext,
-    aad,
-    ciphertext,
-    tag
-  } = loadTestVector();
-  const actual: {
-    ciphertext: Uint8Array;
-    tag: Uint8Array;
-  } = aeadChaCha20Poly1305Seal(key, iv, constant, plaintext, aad);
-  assert.equal(actual.ciphertext, ciphertext, "unequal ciphertexts");
-  assert.equal(actual.tag, tag, "unequal tags");
+  for (const {
+      key,
+      nonce,
+      plaintext,
+      aad,
+      ciphertext,
+      tag
+    } of testVectors.seal) {
+    const actual: {
+      ciphertext: Uint8Array;
+      tag: Uint8Array;
+    } = aeadChaCha20Poly1305Seal(key, nonce, plaintext, aad);
+    assert.equal(actual.ciphertext, ciphertext, "unequal ciphertexts");
+    assert.equal(actual.tag, tag, "unequal tags");
+  }
 });
 
 runIfMain(import.meta);
