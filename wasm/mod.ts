39f2df907ac891cd9a9472057d217088cc3221f9
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-08-28 17:17:56 +0200

    finnna simplify

--- wasm/mod.ts
+++ wasm/mod.ts
@@ -2,14 +2,25 @@ import { loadWasm } from "./loadWasm.ts";
 
 const  { addHeapObject, wasm} = loadWasm();
 
-console.log(wasm);
+console.log("wasm", wasm, "seal arity", wasm.seal.length);
 
 /** Applies AEAD_CHACHA20_POLY1305 to seal given plaintext. */
 export function seal (key: Uint8Array, nonce: Uint8Array,  plaintext: Uint8Array,aad: Uint8Array): { ciphertext: Uint8Array; tag: Uint8Array } {
   const ciphertext: Uint8Array = new Uint8Array(plaintext.length);
   const tag: Uint8Array = new Uint8Array(16);
 
-    wasm.seal(addHeapObject(key), addHeapObject(nonce), addHeapObject(aad), addHeapObject(plaintext), addHeapObject(ciphertext), addHeapObject(tag));
+    wasm.seal(addHeapObject(key), addHeapObject(nonce), addHeapObject(aad), addHeapObject(plaintext), addHeapObject(ciphertext), plaintext.length);
 
     return {ciphertext, tag}
 };
+
+// 
+// /** Applies AEAD_CHACHA20_POLY1305 to seal given plaintext. */
+// export function seal (key: Uint8Array, nonce: Uint8Array,  plaintext: Uint8Array,aad: Uint8Array): { ciphertext: Uint8Array; tag: Uint8Array } {
+//   const ciphertext: Uint8Array = new Uint8Array(plaintext.length);
+//   const tag: Uint8Array = new Uint8Array(16);
+// 
+//     wasm.seal(addHeapObject(key), addHeapObject(nonce), addHeapObject(aad), addHeapObject(plaintext), addHeapObject(ciphertext), addHeapObject(tag));
+// 
+//     return {ciphertext, tag}
+// };
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,46 +5,51 @@ use js_sys::Uint8Array;
 // TRY: pass key, nonce.aad,plaintext as &[u8] -> no need to do the copy_to
 #[wasm_bindgen]
 pub fn seal(
-    key: Uint8Array,
-    nonce: Uint8Array,
-    aad: Uint8Array,
-    plaintext: Uint8Array,
-    ciphertext: Uint8Array,
-    tag: Uint8Array,
-) -> Result<(), JsValue> {
-   let mut _key: Vec<u8> = vec![0; 16];
-   let mut _nonce: Vec<u8> = vec![0; 12];
-   let mut _aad: Vec<u8> = vec![0; aad.length() as usize];
-   let mut _plaintext: Vec<u8> = vec![0; plaintext.length() as usize];
-   let mut _ciphertext: Vec<u8> = vec![0; ciphertext.length() as usize];
+    key: &[u8],
+    nonce: &[u8],
+    aad: &[u8],
+    plaintext: &[u8],
+    mut ciphertext: &mut [u8],
+    mut tag: &mut [u8],
+) -> Result<JsValue, JsValue> {
+    let _tag = encrypt(key, nonce, aad, plaintext, &mut ciphertext).unwrap();
 
-   key.copy_to(&mut _key);
-   nonce.copy_to(&mut _nonce);
-   aad.copy_to(&mut _aad);
-   plaintext.copy_to(&mut _plaintext);
+    // tag.set(&Uint8Array::from(&_tag[..]), 0);
 
-    let _tag = encrypt(
-        &_key,
-        &_nonce,
-         &_aad,
-         &_plaintext,
-         &mut _ciphertext
-     ).unwrap();
-
-    ciphertext.set(&Uint8Array::from(&_ciphertext[..]), 0);
-    tag.set(&Uint8Array::from(&_tag[..]), 0);
-
-    Ok(())
+    // TODO: wrap ciphertext and tag in an object
+    Ok(JsValue::NULL)
 }
 
 // #[wasm_bindgen]
-// pub fn open(
-//     key: JsValue,
-//     nonce: JsValue,
-//     aad: JsValue,
-//     ciphertext: JsValue,
-//     tag: JsValue,
-//     mut plaintext: JsValue,
+// pub fn seal(
+//     key: Uint8Array,
+//     nonce: Uint8Array,
+//     aad: Uint8Array,
+//     plaintext: Uint8Array,
+//     ciphertext: Uint8Array,
+//     tag: Uint8Array,
 // ) -> Result<(), JsValue> {
-//     Ok(decrypt(&key, &nonce, &aad, &ciphertext, &tag, &mut plaintext).unwrap())
+//    let mut _key: Vec<u8> = vec![0; 16];
+//    let mut _nonce: Vec<u8> = vec![0; 12];
+//    let mut _aad: Vec<u8> = vec![0; aad.length() as usize];
+//    let mut _plaintext: Vec<u8> = vec![0; plaintext.length() as usize];
+//    let mut _ciphertext: Vec<u8> = vec![0; ciphertext.length() as usize];
+// 
+//    key.copy_to(&mut _key);
+//    nonce.copy_to(&mut _nonce);
+//    aad.copy_to(&mut _aad);
+//    plaintext.copy_to(&mut _plaintext);
+// 
+//     let _tag = encrypt(
+//         &_key,
+//         &_nonce,
+//          &_aad,
+//          &_plaintext,
+//          &mut _ciphertext
+//      ).unwrap();
+// 
+//     ciphertext.set(&Uint8Array::from(&_ciphertext[..]), 0);
+//     tag.set(&Uint8Array::from(&_tag[..]), 0);
+// 
+//     Ok(())
 // }
